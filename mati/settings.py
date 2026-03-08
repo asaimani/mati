4978f825f94d5b08cb16b857964f2222c11a2a32
asaimani <asaimani1991@gmail.com>
2026-03-08 11:44:17 +0530

    add railway domain to allowed hosts

--- mati/settings.py
+++ mati/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-i1=-9vf#x-e5x95@6p1d@ro6&!j&owd+r!v!z4jcy0gdds5ujf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'mati-production-2bc9.up.railway.app']
+CSFR_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://mati-production-2bc9.up.railway.app']
 
 # Application definition
 
